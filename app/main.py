2cd1439dfd017ad81e2225a06b61fe4b93684a8b
Mizrob <mizi2020@mail.ru>
2024-05-18 17:54:04 +0500

    completing main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
 import os
 from .utils import (ClientActions,
                    load_data, 
@@ -34,3 +34,41 @@ async def upload_file(file: UploadFile = File(...)):
     }
     return response
 
+
+# function saves data to upload file
+@app.post("/save/")
+async def save_data(file: UploadFile = File(...)):
+    # using temporary file to save data
+    with open("temp.csv", "wb") as f:
+        f.write(await file.read())
+    
+    # Load the data from the temporary file
+    data = load_data("temp.csv")
+    
+    aggregated_data = aggregate_client_actions(data)
+    
+    # Removing temporary file
+    os.remove("temp.csv")
+    save_data = os.path.join('uploads\saved_data.csv')
+    
+    # check for data correctness, and return correct HttpResponse 
+    if save_processed_data(aggregated_data, save_data):
+        return HTTPException(status_code=200, detail='File saved successfully')
+    else:
+        return HTTPException(status_code=400, detail='File not saved successfully')
+    
+
+# function filtres client action 
+@app.post("/filter/")
+async def filter_data(client_action: ClientActions, file: UploadFile = File(...)):\
+    # using temporary file to save data
+    with open("temp.csv", "wb") as f:
+        f.write(await file.read())
+    
+    # Load the data from the temporary file
+    data = load_data("temp.csv")
+    
+    filtered_data = filter_client_action(data, client_action)
+    # Removing temporary file
+    os.remove("temp.csv")
+    return filtered_data.to_dict(orient="records")
