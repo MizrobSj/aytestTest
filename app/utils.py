2132601b964a955036bfe29545ce78cc7f4eed43
Mizrob <mizi2020@mail.ru>
2024-05-18 17:30:03 +0500

    created two functions

--- app/utils.py
+++ app/utils.py
@@ -1,2 +1,17 @@
 import pandas as pd 
 from enum import Enum
+
+# enum class which contains actions
+class ClientActions(str, Enum):
+    purchase = 'purchase'
+    add_to_cart = 'add_to_cart'
+    view_product = 'view_product'
+
+# load data from csv file
+def load_data(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+def aggregate_client_actions(data):
+    actions = data.groupby('client_id').size().reset_index(name='number_of_actions')
+    return actions
