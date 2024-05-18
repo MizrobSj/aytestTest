92d354a247ca622df59e44fa964775fd5b6cc095
Mizrob <mizi2020@mail.ru>
2024-05-18 17:41:18 +0500

    creating all utils function

--- app/utils.py
+++ app/utils.py
@@ -12,6 +12,31 @@ def load_data(file_path):
     df = pd.read_csv(file_path)
     return df
 
+# aggregate data by client_id
 def aggregate_client_actions(data):
     actions = data.groupby('client_id').size().reset_index(name='number_of_actions')
     return actions
+
+# filtering data by client actions
+def filter_client_action(data, client_action: str):
+    actions = data[data['action'] == client_action]
+    return actions
+
+# function to return average actions and active clients
+def analyze_client_behavior(data):
+    # using mean to calculate average actions made by clients
+    average_actions = data.groupby('client_id').size().mean()
+    # using pandas sort values function to return top 5 active clients
+    client_with_most_actions = aggregate_client_actions(data).sort_values(by='number_of_actions', ascending=False).head(2)
+    return average_actions, client_with_most_actions
+
+# saving aggregate data to csv file
+def save_processed_data(data, output_file):
+    try:
+        # saving data in csv format
+        data.to_csv(output_file)
+        return True
+    # checking for permission access
+    except PermissionError:
+        print("User don't have access to save file in this Path:", output_file)
+        return False
